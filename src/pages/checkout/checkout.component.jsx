2decfc3e36431578232a5e6bd30dcd4f2be466c2
Abad Joshua <abad.joshuad@gmail.com>
2019-09-21 10:31:46 +0800

    Changes has been done

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect'
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component'
+import StripeCheckoutButton from '../../components/stripe/stripe-button.component'
 import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems,totalCost}) =>(
@@ -31,6 +32,11 @@ const CheckoutPage = ({cartItems,totalCost}) =>(
         <div className='total'>
             <span>TOTAL:${totalCost}</span>
         </div>
+        <div className='test-warning'>*Please use the following test credit card for payments*
+            <br/>
+            4242 4242 4242 4242 -Exp:01/20 - CVV:123
+        </div>
+            <StripeCheckoutButton price={totalCost} />
     </div>
 )
 
